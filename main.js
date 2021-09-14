3cdd375201a889b64215a6c058a72cc4ebf2b3e7
Harrys Kavan <harrys.kavan@pm.me>
2021-09-14 18:21:42 +0200

    add basic scene

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import './style.css'
+import { createScene } from "./scene";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const el = document.getElementById("el");
+
+createScene(el);
