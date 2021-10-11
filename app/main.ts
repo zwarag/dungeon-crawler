0d3385c1f600a8653a9986a40203dadb9fe5ad2f
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-10-11 15:21:19 +0200

    refactor highscore page to single page

--- app/main.ts
+++ app/main.ts
@@ -1,20 +1,37 @@
 import "./style.scss";
 import { Game } from "./src/game";
 
-const highscore = document.querySelector<HTMLDivElement>("#highscore");
 const app = document.querySelector<HTMLDivElement>("#app");
+const highscore = document.querySelector<HTMLDivElement>("#highscore");
+const startScreen = document.querySelector<HTMLDivElement>("#start-screen");
 const element = document.querySelector<HTMLCanvasElement>("#element");
-const button = document.querySelector<HTMLButtonElement>("#startbutton");
+const startButton = document.querySelector<HTMLButtonElement>("#startbutton");
+const highscoreButton =
+  document.querySelector<HTMLButtonElement>("#highscoreButton");
+const backButton = document.querySelector<HTMLButtonElement>("#backbutton");
 
 function startGame() {
   app?.classList.remove("d-none");
   highscore?.classList.add("d-none");
-  button?.classList.add("d-none");
+  startButton?.classList.add("d-none");
+  highscoreButton?.classList.add("d-none");
   if (element) {
     new Game(element);
   }
 }
 
-button?.addEventListener("click", startGame);
+function displayHighscore() {
+  startScreen?.classList.add("d-none");
+  highscore?.classList.remove("d-none");
+}
+
+function backToStartScreen() {
+  startScreen?.classList.remove("d-none");
+  highscore?.classList.add("d-none");
+}
+
+startButton?.addEventListener("click", startGame);
+highscoreButton?.addEventListener("click", displayHighscore);
+backButton?.addEventListener("click", backToStartScreen);
 
 // startGame();
