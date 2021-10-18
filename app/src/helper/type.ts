3e9824aaee6d2f8caca6d40d30ebd0e54b8770d9
Harrys Kavan <harrys.kavan@pm.me>
2021-10-18 13:10:33 +0200

    refactor: extract a baseclass from character+enemy, rename charater to player

--- app/src/helper/type.ts
+++ app/src/helper/type.ts
@@ -1,1 +1,6 @@
 export type Grid = string[][];
+export type Health = number;
+export type Experience = number;
+export type Damage = number;
+export type Accuracy = number;
+export type Range = number;
