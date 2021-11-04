d3c9d8b0d7c15a0e90d7f762a92f19ca612191f9
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2021-11-04 23:01:43 +0100

    Vampire included

--- app/src/helper/enemy.ts
+++ app/src/helper/enemy.ts
@@ -1,3 +1,3 @@
 // placeholder enemies
-export const ENEMY_TYPE_LIST = ['ZOMBIE', 'WARROK', 'DEVIL'] as const;
+export const ENEMY_TYPE_LIST = ['ZOMBIE', 'WARROK', 'VAMPIRE'] as const;
 export type ENEMY = typeof ENEMY_TYPE_LIST[number];
